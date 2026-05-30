411781811f1de0c55635a7ff8f25217ebe57812f
Your Name <a.senhaji@genidochayat.ma>
2026-05-30 13:46:26 +0100

    Use relative API URL

--- frontend/public/js/api.js
+++ frontend/public/js/api.js
@@ -1,5 +1,5 @@
 // API helper functions
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "/api";
 
 // Get token from localStorage
 const getToken = () => localStorage.getItem("token");
